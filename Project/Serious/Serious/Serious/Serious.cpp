6c7b83f7b2dc2bb8cafa00d764b45172d2f9bdd3
EdocDyne <bandyck@gmail.com>
2020-08-31 17:26:22 +0900

    20200831-SE

--- Project/Serious/Serious/Serious/Serious.cpp
+++ Project/Serious/Serious/Serious/Serious.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <iostream>
 #include <CommCtrl.h>
+#include <stack>
+#include <algorithm>
+#include <queue>
+#include <functional>
 using namespace std;
 #define MAX_LOADSTRING 100
 
@@ -91,7 +95,7 @@ void RenderSet(HDC hdc);
 #define INF INT_MAX-1
 enum Node_stat : int
 {
-	Node_start = 0, Node_end, Node_ground, Node_tile, Node_fog, Node_tower, Node_open, Node_close, Node_calculated, Node_path
+	Node_start = 0, Node_end, Node_open, Node_close, Node_calculated, Node_ground, Node_tile, Node_fog, Node_path, Node_tower
 };
 class Node
 {
@@ -99,22 +103,58 @@ public:
 	POINT center;
 	int state;
 	HIMAGELIST hIL;
+	Node * closest_Node;
+	int G;
+	int H;
+	int F;
+	Node() { InitNode(); }
+	void InitNode()
+	{
+		G = INF;
+		H = INF;
+		F = INF;
+		closest_Node = NULL;
+		state = Node_open;
+	}
 };
+bool operator<(const Node & node1, const Node & node2)
+{
+	if (node1.F == node2.F)
+		return node1.G < node2.G;
+	return node1.F > node2.F;
+}
+inline bool operator==(const POINT & p1, const POINT & p2)
+{
+	if (p1.x == p2.x && p1.y == p2.y)
+		return true;
+	return false;
+}
+vector<Node> oc;
 Node Map[MAPSIZE][MAPSIZE];
 void NodeInit();
 void RenderNode(HDC hdc);
+static POINT startPoint;
+static POINT endPoint;
+void DrawMon(HDC hdc);
+static vector<POINT> MonPath;
+static Node MonSter;
+bool AStarPathFinding(const POINT & startPoint, const POINT & endPoint);
+void AStarCalculateNode(const POINT & startPoint, const POINT & endPoint, const POINT & curPoint);
+void AStarPathMaking(const POINT & endPoint);
 // >> NODE end :
 void StartInit(HWND hWnd);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-
+	static int time = 0;
     switch (message)
     {
 	case WM_CREATE:
-		STAGE = 1;
+		STAGE = 12;
 		SetInit(hWnd);
 		NodeInit();
 		StartInit(hWnd);
+		MonSter.center.x = Map[0][0].center.x;
+		MonSter.center.y = Map[0][0].center.y;
 		// ÄÜ¼Ö
 		//AllocConsole();
 		//freopen("CONOUT$", "wt", stdout);
@@ -124,17 +164,65 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             int wmId = LOWORD(wParam);
             switch (wmId)
             {
+				enum Start
+				{
+					start = 100
+				};
+			case start:
+			{
+				while (1)
+				{
+					if (!oc.empty())
+						AStarPathFinding(startPoint, endPoint);
+					else
+					{
+						if (Map[MAPSIZE - 1][MAPSIZE - 1].closest_Node != NULL)
+						{
+							AStarPathMaking(endPoint);
+							break;
+						}
+					}
+				}
+				reverse(MonPath.begin(), MonPath.end());
+				MonPath.insert(MonPath.begin(), Map[0][0].center);
+				MonPath.push_back(Map[MAPSIZE - 1][MAPSIZE - 1].center);
+				for (int i = 0; i < MonPath.size(); i++)
+				{
+					cout << MonPath[i].x << ' ' << MonPath[i].y << endl;
+				}
+				SetTimer(hWnd, 1, 100, NULL);
+			}
+			InvalidateRgn(hWnd, NULL, TRUE);
             default:
                 return DefWindowProc(hWnd, message, wParam, lParam);
             }
         }
         break;
+	case WM_TIMER:
+	{
+		if (MonPath[time / 64 + 1].y == MonSter.center.y)
+		{
+			if (MonPath[time / 64 + 1].x > MonSter.center.x)
+				MonSter.center.x += 16;
+			else MonSter.center.x -= 16;
+		}
+		else if (MonPath[time / 64 + 1].x == MonSter.center.x)
+		{
+			if (MonPath[time / 64 + 1].y > MonSter.center.y)
+				MonSter.center.y += 16;
+			else MonSter.center.y -= 16;
+		}
+		time += 16;
+		InvalidateRgn(hWnd, NULL, TRUE);
+	}
+	break;
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
 			RenderSet(hdc);
 			RenderNode(hdc);
+			DrawMon(hdc);
             EndPaint(hWnd, &ps);
         }
         break;
@@ -201,14 +289,26 @@ void NodeInit()
 			Map[i][j].center.x = 32 + NODESIZE / 2 + NODESIZE*j;
 			Map[i][j].center.y = 32 + NODESIZE / 2 + NODESIZE*i;
 			Map[i][j].state = Node_fog;
-			Map[i][j].hIL = ImageList_LoadImage(NULL, _T("Resource/MapNode.bmp"), 64, 1, CLR_NONE, IMAGE_BITMAP, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+			Map[i][j].hIL = ImageList_LoadImage(NULL, _T("Resource/MapNodeTmp.bmp"), 64, 1, CLR_NONE, IMAGE_BITMAP, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
 		}
 	for (i = 12 - STAGE; i < MAPSIZE; i++)
 		for (j = 12 - STAGE; j < MAPSIZE; j++)
 			Map[i][j].state = Node_ground;
 	Map[12 - STAGE][12 - STAGE].state = Node_start;
-	Map[MAPSIZE - 2][MAPSIZE - 2].state = Node_tile;
+	Map[12 - STAGE][12 - STAGE].G = 0;
+	startPoint.x = Map[12 - STAGE][12 - STAGE].center.x / NODESIZE - 1;
+	startPoint.y = Map[12 - STAGE][12 - STAGE].center.y / NODESIZE - 1;
 	Map[MAPSIZE - 1][MAPSIZE - 1].state = Node_end;
+	endPoint.x = Map[MAPSIZE - 1][MAPSIZE - 1].center.x / NODESIZE - 1;
+	endPoint.y = Map[MAPSIZE - 1][MAPSIZE - 1].center.y / NODESIZE - 1;
+	Map[0][MAPSIZE - 1].state = Node_tile;
+	Map[1][MAPSIZE - 2].state = Node_tile;
+	Map[2][MAPSIZE - 3].state = Node_tile;
+
+
+	oc.clear();
+	make_heap(oc.begin(), oc.end());
+	oc.push_back(Map[12 - STAGE][12 - STAGE]);
 }
 void RenderNode(HDC hdc)
 {
@@ -216,7 +316,6 @@ void RenderNode(HDC hdc)
 		for (int j = 0; j < MAPSIZE; j++)
 			ImageList_Draw(Map[i][j].hIL, Map[i][j].state, hdc, (Map[i][j].center.x - NODESIZE / 2), (Map[i][j].center.y - NODESIZE / 2), ILD_TRANSPARENT);
 }
-
 void StartInit(HWND hWnd)
 {
 	HWND StartButton;
@@ -224,4 +323,101 @@ void StartInit(HWND hWnd)
 	StartButtonMenu = (HMENU)100;
 	StartButton = CreateWindow(TEXT("BUTTON"), TEXT("START"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
 		1408, 800, 128, 128, hWnd, StartButtonMenu, hInst, NULL);
+}
+bool AStarPathFinding(const POINT & startPoint, const POINT & endPoint)
+{
+	static POINT curPoint;
+	pop_heap(oc.begin(), oc.end());
+	curPoint.x = oc.back().center.x / NODESIZE - 1;
+	curPoint.y = oc.back().center.y / NODESIZE - 1;
+	oc.pop_back();
+	AStarCalculateNode(startPoint, endPoint, curPoint);
+	if (Map[curPoint.y][curPoint.x].state != Node_start)
+		Map[curPoint.y][curPoint.x].state = Node_close;
+	return true;
+}
+void AStarCalculateNode(const POINT & startPoint, const POINT & endPoint, const POINT & curPoint)
+{
+	POINT tmpPoint;
+
+	for (int i = 0; i < 4; i++)
+	{
+		switch (i)
+		{
+		case 0:
+			tmpPoint.x = curPoint.x - 1;
+			tmpPoint.y = curPoint.y;
+			break;
+		case 1:
+			tmpPoint.x = curPoint.x;
+			tmpPoint.y = curPoint.y - 1;
+			break;
+		case 2:
+			tmpPoint.x = curPoint.x + 1;
+			tmpPoint.y = curPoint.y;
+			break;
+		case 3:
+			tmpPoint.x = curPoint.x;
+			tmpPoint.y = curPoint.y + 1;
+			break;
+		}
+		if (tmpPoint == endPoint)
+		{
+			Map[tmpPoint.y][tmpPoint.x].closest_Node = &Map[curPoint.y][curPoint.x];
+			Map[tmpPoint.y][tmpPoint.x].state = Node_end;
+			oc.clear();
+			break;
+		}
+		if (tmpPoint.x < 0 || tmpPoint.x == MAPSIZE || tmpPoint.y < 0 || tmpPoint.y == MAPSIZE ||
+			Map[tmpPoint.y][tmpPoint.x].state == Node_close ||
+			Map[tmpPoint.y][tmpPoint.x].state == Node_tile ||
+			Map[tmpPoint.y][tmpPoint.x].state == Node_fog ||
+			Map[tmpPoint.y][tmpPoint.x].state == Node_tower ||
+			Map[tmpPoint.y][tmpPoint.x].state == Node_start)
+			continue;
+		int Gval = Map[tmpPoint.y][tmpPoint.x].G + 1;
+		int Hval = (endPoint.y - curPoint.y) + (endPoint.x - curPoint.x);
+
+		if (Map[tmpPoint.y][tmpPoint.x].G > Gval)
+			Map[tmpPoint.y][tmpPoint.x].G = Gval;
+		else
+			Gval = Map[tmpPoint.y][tmpPoint.x].G;
+
+		if (Map[tmpPoint.y][tmpPoint.x].H > Hval)
+			Map[tmpPoint.y][tmpPoint.x].H = Hval;
+		else
+			Hval = Map[tmpPoint.y][tmpPoint.x].H;
+
+		if (Map[tmpPoint.y][tmpPoint.x].F > Gval + Hval)
+		{
+			Map[tmpPoint.y][tmpPoint.x].F = Gval + Hval;
+			Map[tmpPoint.y][tmpPoint.x].closest_Node = &Map[curPoint.y][curPoint.x];
+		}
+
+		if (Map[tmpPoint.y][tmpPoint.x].state != Node_calculated)
+		{
+			Map[tmpPoint.y][tmpPoint.x].state = Node_calculated;
+			oc.push_back(Map[tmpPoint.y][tmpPoint.x]);
+			push_heap(oc.begin(), oc.end());
+		}
+	}
+}
+void AStarPathMaking(const POINT & endPoint)
+{
+	Node * cur_Node = Map[endPoint.y][endPoint.x].closest_Node;
+	while (cur_Node->state != Node_start)
+	{
+		MonPath.push_back(cur_Node->center);
+		cur_Node->state = Node_path;
+		cur_Node = cur_Node->closest_Node;
+	}
+}
+void DrawMon(HDC hdc)
+{
+	static HBRUSH MonBrush = CreateSolidBrush(RGB(255, 0, 0));
+	HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, MonBrush);
+	const int left = MonSter.center.x - NODESIZE / 2;
+	const int top = MonSter.center.y - NODESIZE / 2;
+	Rectangle(hdc, left, top, left + NODESIZE, top + NODESIZE);
+	SelectObject(hdc, oldBrush);
 }
